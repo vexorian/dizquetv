05a8b2c4afdf5b95503a99c240c3154d603da16e
vexorian <vexorian@gmail.com>
2020-08-15 18:16:23 -0400

    Improve version tab, include UI and ffmpeg versions. Channels in m3u get sorted by channel number because apparently that's important.

---
+++ src/ffmpeg-info.js
@@ -0,0 +1,26 @@
+const exec = require('child_process').exec;
+
+class FFMPEGInfo {
+    constructor(opts) {
+        this.ffmpegPath = opts.ffmpegPath
+    }
+    async getVersion() {
+        try {
+            let s = await new Promise( (resolve, reject) => {
+                exec( `"${this.ffmpegPath}" -version`, function(error, stdout, stderr){
+                    if (error !== null) {
+                        reject(error);
+                    } else {
+                        resolve(stdout);
+                    }
+                });
+            });
+            return s.match( /version ([^\s]+) Copyright/ )[1];
+        } catch (err) {
+            console.error("Error getting ffmpeg version", err);
+            return "Error";
+        }
+    }
+}
+
+module.exports = FFMPEGInfo
