2efe1c0f4825260506335b9d129f49270126083b
vexorian <vexorian@gmail.com>
2020-08-16 21:44:46 -0400

    Improve Dockerfiles, but the build is slower regardless of caching, because nexe is hideously limited, we really need to find a better way to package these things

--- make_dist.sh
+++ make_dist.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+MODE=${1:-all}
 WIN64=dizquetv-win-x64.exe
 WIN32=dizquetv-win-x86.exe
 MACOSX=dizquetv-macos-x64
-LINUX64=dizquetv-linux-x64
+LINUX64=${LINUXBUILD:-dizquetv-linux-x64}
 
 rm -R ./dist/*
 npm run build
@@ -10,18 +11,23 @@ npm run compile
 cp -R ./web ./dist/web
 cp -R ./resources ./dist/
 cd dist
-nexe -r "./**/*" -t windows-x64-12.18.2 --output $WIN64
-mv $WIN64 ../
-nexe -r "./**/*" -t linux-x64-12.16.2  --output $LINUX64
-mv $LINUX64 ../
-nexe -r "./**/*" -t mac-x64-12.18.2 --output $MACOSX
-mv $MACOSX ../
-nexe -r "./**/*" -t windows-x86-12.18.2 --output $WIN32
-mv ../$WIN64 ./
-mv ../$LINUX64 ./
-mv ../$MACOSX ./
-echo dist/$WIN64
+if [ "$MODE" == "all" ]; then
+    nexe -r "./**/*" -t windows-x64-12.18.2 --output $WIN64
+    mv $WIN64 ../
+    nexe -r "./**/*" -t mac-x64-12.18.2 --output $MACOSX
+    mv $MACOSX ../
+    nexe -r "./**/*" -t windows-x86-12.18.2 --output $WIN32
+    mv $WIN32 ../
+fi
+
+nexe  -r "./**/*" -t linux-x64-12.16.2 --output $LINUX64 || exit 1
 echo dist/$LINUX64
-echo dist/$MACOSX
-echo dist/$WIN32
+if [ "$MODE" == "all" ]; then
+    mv ../$WIN64 ./
+    mv ../$WIN32 ./
+    mv ../$MACOSX ./
+    echo dist/$WIN64
+    echo dist/$MACOSX
+    echo dist/$WIN32
+fi
 
