27823ac66503e3adfbd0086f2ddf33099bb87eb8
vexorian <vexorian@gmail.com>
2020-09-05 18:37:01 -0400

    0.0.66

--- src/constants.js
+++ src/constants.js
@@ -4,5 +4,5 @@ module.exports = {
     STEALTH_DURATION: 5 * 60* 1000,
     TVGUIDE_MAXIMUM_FLEX_DURATION : 6 * 60 * 60 * 1000,
 
-    VERSION_NAME: "0.0.66-prerelease"
+    VERSION_NAME: "0.0.66"
 }
