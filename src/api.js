c114cab269e431374ae0b79d445bec41849b99b4
vexorian <vexorian@gmail.com>
2020-09-26 12:53:32 -0400

    #110 expose url-tvg and x-tvg-url fields in m3u

--- src/api.js
+++ src/api.js
@@ -453,7 +453,8 @@ function api(db, channelDB, xmltvInterval,  guideService ) {
         res.type('text')
         let channels = await channelDB.getAllChannels();
         channels.sort((a, b) => { return a.number < b.number ? -1 : 1 })
-        var data = "#EXTM3U\n"
+        let tvg = `${req.protocol}://${req.get('host')}/api/xmltv.xml`
+        var data = `#EXTM3U url-tvg="${tvg}" x-tvg-url="${tvg}"\n`;
         for (var i = 0; i < channels.length; i++) {
           if (channels[i].stealth!==true) {
             data += `#EXTINF:0 tvg-id="${channels[i].number}" tvg-chno="${channels[i].number}" tvg-name="${channels[i].name}" tvg-logo="${channels[i].icon}" group-title="dizqueTV",${channels[i].name}\n`
