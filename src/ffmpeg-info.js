a3b74e8af53866a5aafc2d31eed01883a399052c
vexorian <vexorian@gmail.com>
2021-01-09 10:01:59 -0400

    #219 Better ffmpeg version handling.

--- src/ffmpeg-info.js
+++ src/ffmpeg-info.js
@@ -15,7 +15,12 @@ class FFMPEGInfo {
                     }
                 });
             });
-            return s.match( /version ([^\s]+) Copyright/ )[1];
+            var m = s.match( /version\s+([^\s]+)\s+[^\s].*Copyright/ )
+            if (m == null) {
+                console.error("ffmpeg -version command output not in the expected format: " + s);
+                return s;
+            }
+            return m[1];
         } catch (err) {
             console.error("Error getting ffmpeg version", err);
             return "Error";
